77d1eafb5962d67e780f1cb81ba3c19288b75bc2
NelsonDJCR <dev@nelsondjcr.com>
2023-06-17 10:51:00 -0500

    pages

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+	import Nav from '../componets/Nav.svelte';
+</script>
+
+<Nav />
+<slot />
+<h1 class="w-100 bg-red">a</h1>
+
+<style>
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
+</style>
