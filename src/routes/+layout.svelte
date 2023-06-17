20fb357ecb4ca47a06facdd4cf802fc5e5e5c3f6
NelsonDJCR <dev@nelsondjcr.com>
2023-06-17 10:54:52 -0500

    install tailwind

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,7 @@
 <script>
+	import '../app.css';
 	import Nav from '../componets/Nav.svelte';
 </script>
-
 <Nav />
 <slot />
-<h1 class="w-100 bg-red">a</h1>
-
-<style>
-	@tailwind base;
-	@tailwind components;
-	@tailwind utilities;
-</style>
+<h1 class="w-100 bg-red-100 text-center">a</h1>
